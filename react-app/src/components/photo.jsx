a64f9c82dbff49881125beb77d598d4a688a4087
Courtney Noonan <courtneynoonan@me.com>
2018-01-09 11:48:41 -0500

    refactoring photo index layout to use flexbox to display the photos

--- react-app/src/components/photo.jsx
+++ react-app/src/components/photo.jsx
@@ -5,20 +5,24 @@ import styles from '../../styles/photo_index.scss'
 
 // photo as displayed in a list
 export default function Photo(props) {
-  return (
-    <div className={styles['photo-div']}>
-      <Link className={styles['photo-link']} to={"/photos/" + props.photo.id}>
-        <img className={styles['photo']} src={props.photo.picture} />
-      </Link>
-      <h3 className={styles['photo-title']}>{props.photo.name}</h3>
-      <p className={styles['photo-desc']}>{props.photo.description}</p>
-      <div className={styles['user']}>
-        <span>
-          <small>by: {props.photo.user.username}
-          </small><br></br>
-        <small className={styles['photo-info']}>posted {formatUnixShortDate(props.photo.date_created)} </small>
-      </span>
-      </div>
-    </div>
-    )
+	return (
+		<div className={styles['photo-div']}>
+			<Link className={styles['photo-link']} to={'/photos/' + props.photo.id}>
+				<img className={styles['photo']} src={props.photo.picture} />
+			</Link>
+			<div>
+				<h3 className={styles['photo-title']}>{props.photo.name}</h3>
+				<p className={styles['photo-desc']}>{props.photo.description}</p>
+				<div className={styles['user']}>
+					<span>
+						<small>by: {props.photo.user.username}</small>
+						<br />
+						<small className={styles['photo-info']}>
+							posted {formatUnixShortDate(props.photo.date_created)}{' '}
+						</small>
+					</span>
+				</div>
+			</div>
+		</div>
+	)
 }
